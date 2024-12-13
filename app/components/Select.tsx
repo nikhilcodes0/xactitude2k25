b5731af6cd8abe5abb1d09390e914848957f9327
4all3n <hpsingh7146@gmail.com>
2024-12-13 22:32:02 +0530

    fixed the styling for the reg page

--- app/components/Select.tsx
+++ app/components/Select.tsx
@@ -6,10 +6,10 @@ import Image, { StaticImageData } from 'next/image'
 const Select = ({title, image}: {title: string, image: string | StaticImageData}) => {
   return (
     <div className='bg-black'>
-      <div className='w-[220px]'>
+      <div className='max-w-[220px]'>
         <Image src={image} alt="bg1" className="w-full h-full object-cover"/>
         <div className='bg-black text-white'>
-          <h3 className="text-4xl">{title}</h3>  
+          <h3 className="text-xl my-1">{title}</h3>  
         </div>
       </div>
     </div>
--- app/reg/page.tsx
+++ app/reg/page.tsx
@@ -5,17 +5,25 @@ import cine from '../assets/cine.png'
 import fc24 from '../assets/fc24.png'
 import quizz from '../assets/quizz.png'
 
+let events = [
+    {id: 1, title: 'Weblynx', image: weblynx},
+    {id: 2, title: 'Cinephoria', image: cine},
+    {id: 3, title: 'FC24', image: fc24},
+    {id: 4, title: 'Quizz Buzz', image: quizz}
+]
+
+let noEvent = events.length
 
 
 const Reg = () => {
   return (
     <div className='bg-black text-white font-sans'>
-        <div >
-            <h1 className='text-[2.2rem] font-bold'>SELECT YOUR EVENTS -</h1>
-            <hr className=''/>
+        <div className=''>
+            <h1 className='text-[2rem] font-bold pt-7 pb-3 px-3'>SELECT YOUR EVENTS -</h1>
+            <hr className='mx-3 opacity-60'/>
         </div>
 
-        <div className='grid grid-cols-2 gap-x-16 gap-y-3 bg-black text-white'>
+        <div className='grid grid-cols-2 gap-3 mx-3 my-6 bg-black text-white'>
 
             <Select 
             title='Weblynx'
@@ -58,17 +66,31 @@ const Reg = () => {
             />
 
         </div>
+        
+        <div className='flex gap-2 mx-3 items-center'>
+            <input type="radio" checked className='w-5 h-5 accent-[#2FFF60]' />
+            <label className='text-[#2FFF60] text-xl'> {noEvent} events selected</label>
+        </div>
 
-        <div>
-            <h1>ENERT YOUR DETAILS</h1>
+        <div className='mx-3 mt-16 bg-black text-white'>
+            <h1 className='text-[2rem] font-bold mb-16'>ENERT YOUR DETAILS</h1>
 
-            <div className='flex flex-col gap-4 bg-black text-white'>
-                <input type="text" />
-                <input type="text" />
-                <input type="text" />
-                <input type="text" />
+            <div className='flex flex-col gap-4 text-black'>
+                <input type="text" placeholder='Full Name' className='p-2 rounded-md border-b-[5px] border-opacity-50 border-black outline-none'/>
+                <input type="text" placeholder='Institution' className='p-2 rounded-md border-b-[5px] border-opacity-50 border-black outline-none' />
+                <input type="text" placeholder='Whatsapp Number' className='p-2 rounded-md border-b-[5px] border-opacity-50 border-black outline-none' />
+                <input type="text" placeholder='Email ID' className='p-2 rounded-md border-b-[5px] border-opacity-50 border-black outline-none' />
             </div>
         </div>
+        <hr className='mx-3 opacity-60 mt-16' />
+
+        <div className='mx-3 mt-16 bg-black text-white'>
+            <h1 className='text-[2rem] font-bold mb-16 text-center'>GROUP EVENTS</h1>
+
+            <div className='flex flex-col gap-4 bg-black text-white'>
+
+            </div>    
+        </div>            
 
     </div>
   )
