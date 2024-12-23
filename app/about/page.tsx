71ae0655e48edb577a1f14bde105d64f483d9bf7
nikhilcodes0 <nk7488577732@gmail.com>
2024-12-23 21:10:16 +0530

    changes navlinks

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,54 +1,99 @@
 "use client";
 
-import React, { useState } from "react";
-import Image from 'next/image';
-import insta from '../assets/insta.png';
-import mail from '../assets/mail.png';
-
+import React from "react";
+import Image from "next/image";
+import insta from "../assets/insta.png";
+import mail from "../assets/mail.png";
+import Nav from "../components/Nav";
 
 function copyPhoneNumber() {
-    const phoneNumber = '+919988776655';
-    navigator.clipboard.writeText(phoneNumber)
-      .then(() => {
-        alert('Phone number copied to clipboard!');
-      })
-      .catch(err => {
-        console.error('Failed to copy text: ', err);
-      });
-  }
-
+  const phoneNumber = "+919988776655";
+  navigator.clipboard
+    .writeText(phoneNumber)
+    .then(() => {
+      alert("Phone number copied to clipboard!");
+    })
+    .catch((err) => {
+      console.error("Failed to copy text: ", err);
+    });
+}
 
 const About = () => {
-  
-
   return (
-    <div className="bg-black text-white font-sans mx-6">
-        <h1 className="text-4xl text-center mt-16 mb-10 font-bold">About College</h1>
-        <p className="text-justify mt-3 mb-7 font-inter">Kristu Jayanti College, founded in 1999, is managed by "BODHI NIKETAN TRUST", formed by the members of St. Joseph Province of the Carmelites of Mary Immaculate (CMI).<br/><br />
-        The College is affiliated to Bengaluru North University and is reaccredited with grade 'A++' in 2021 by NAAC in the Third Cycle of Accreditation. In the NIRF 2024, Our college has been ranked 60th place in the top 100 colleges in the country.<br/><br />
-        In the India Today - MDRA survey 2024, Kristu Jayanti College, Bengaluru is consecutively ranked as the Best Emerging College of the Century at National Level for Commerce, Science, Arts and Social Work. At the National level, the survey ranked the college as 4th Best in BCA and it is also ranked 3rd Best in BCA programmes among the colleges in Bengaluru.
+    <div>
+      <Nav/>
+      <div className="bg-black text-white font-sans mx-6 mt-32">
+        <h1 className="text-4xl text-center mt-16 mb-10 font-bold">
+          About College
+        </h1>
+        <p className="text-justify mt-3 mb-7 font-inter">
+          Kristu Jayanti College, founded in 1999, is managed by &quot;BODHI
+          NIKETAN TRUST&quot; formed by the members of St. Joseph Province of
+          the Carmelites of Mary Immaculate (CMI).
+          <br />
+          <br />
+          The College is affiliated to Bengaluru North University and is
+          reaccredited with grade &quot;A++&quot; in 2021 by NAAC in the Third
+          Cycle of Accreditation. In the NIRF 2024, Our college has been ranked
+          60th place in the top 100 colleges in the country.
+          <br />
+          <br />
+          In the India Today - MDRA survey 2024, Kristu Jayanti College,
+          Bengaluru is consecutively ranked as the Best Emerging College of the
+          Century at National Level for Commerce, Science, Arts and Social Work.
+          At the National level, the survey ranked the college as 4th Best in
+          BCA and it is also ranked 3rd Best in BCA programmes among the
+          colleges in Bengaluru.
         </p>
-        <hr className="opacity-30"/>
+        <hr className="opacity-30" />
         <h1 className="text-4xl text-center my-7 font-bold">About Xactitude</h1>
         <p className="text-justify my-3 font-inter">
-        Xactitude is the flagship National IT Fest of Department of Computer Science (UG), hosted annually as a stage for colleges to compete in a wide array of events celebrating technological innovation and excellence. This fest aims to uncover and nurture talent in diverse domains of Computer Science, equipping participants to shape the future.<br/><br/>
-        The theme for Xactitude'25, “Revitalizing Tech for Tomorrow,” invites future developers to explore and build technology through sustainable, ethical, and human-centric approaches. It focuses on breathing new life into the current tech landscape to build a future driven by ingenuity, inclusivity, and accountability.<br/><br />
-        At Xactitude, it is not just about participating but it is about going the extra mile to showcase your best self and be a trailblazer in tomorrow's tech revolution!
+          Xactitude is the flagship National IT Fest of Department of Computer
+          Science (UG), hosted annually as a stage for colleges to compete in a
+          wide array of events celebrating technological innovation and
+          excellence. This fest aims to uncover and nurture talent in diverse
+          domains of Computer Science, equipping participants to shape the
+          future.
+          <br />
+          <br />
+          The theme for Xactitude 25, “Revitalizing Tech for Tomorrow,” invites
+          future developers to explore and build technology through sustainable,
+          ethical, and human-centric approaches. It focuses on breathing new
+          life into the current tech landscape to build a future driven by
+          ingenuity, inclusivity, and accountability.
+          <br />
+          <br />
+          At Xactitude, it is not just about participating but it is about going
+          the extra mile to showcase your best self and be a trailblazer in
+          tomorrow&apos;s tech revolution!
         </p>
         <hr className="my-7 opacity-30" />
         <div className="flex justify-between my-16">
-            <div className="">
-                <p className="font-bold my-2 font-inter">Contact event organizer?</p>
-                <p className="my-2 font-inter">Give us a ring</p>
-                <p className="text-2xl font-bold my-2 text-[#40E0D0] " onClick={copyPhoneNumber}>+919988776655</p>
-            </div>
-            <div className="flex flex-col  ">
-                <a href="" className="my-2 font-inter flex gap-1 "> <Image src={insta} alt="" className="w-7 invert"/> Instagram</a>
-                <a href="" className="font-inter flex gap-1 pl-[2px]"> <Image src={mail} alt="" className="w-6 invert "/> Email</a>
-            </div>
+          <div className="">
+            <p className="font-bold my-2 font-inter">
+              Contact event organizer?
+            </p>
+            <p className="my-2 font-inter">Give us a ring</p>
+            <p
+              className="text-2xl font-bold my-2 text-[#40E0D0] cursor-pointer"
+              onClick={copyPhoneNumber}
+            >
+              +919988776655
+            </p>
+          </div>
+          <div className="flex flex-col  ">
+            <a href="" className="my-2 font-inter flex gap-1 ">
+              {" "}
+              <Image src={insta} alt="" className="w-7 invert" /> Instagram
+            </a>
+            <a href="" className="font-inter flex gap-1 pl-[2px]">
+              {" "}
+              <Image src={mail} alt="" className="w-6 invert " /> Email
+            </a>
+          </div>
         </div>
+      </div>
     </div>
-
   );
 };
 
--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -9,17 +9,22 @@ import Link from 'next/link'
 import { useState } from 'react'
 
 
+
 const Nav = () => {
+  
   const [isOpen, setIsOpen] = useState(false)
   const toggleMenu = () => {
     setIsOpen(!isOpen)
   }
+  
   return (
     <div className='flex justify-center '>
         <nav className='mx-auto px-6 fixed w-full top-1 py-2 z-[61] bg-white-900 rounded-md bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-10 border border-transparent'>
-            <div className='flex m-2 gap-1 hover:cursor-pointer justify-between items-center w-[95%]' onClick={toggleMenu}>
-                <Image src={logo} alt='' className='w-[90px] h-full'/>
-                <Image src={isOpen ? cancel : menu_01} alt="" className='w-8 z-[62]' />
+            <div className='flex m-2 gap-1 hover:cursor-pointer justify-between items-center w-[95%]' >
+                <Link href={'/'}>
+                  <Image src={logo} alt='' className='w-[90px] h-full' />
+                </Link>
+                <Image src={isOpen ? cancel : menu_01} alt="" className='w-8 z-[62]' onClick={toggleMenu}/>
             </div>
             <div className={`flex absolute gap-20 flex-col bg-black -top-2 overflow-hidden z-[61] w-full h-screen pt-32 -left-full transition-all duration-300 ease-in-out ${isOpen ? 'translate-x-full' : ''}`}>
               <Link href="/" className={`mx-8 relative transition-all duration-1000 ease-in-out ${isOpen ? 'left-0' : '-left-40'}`}>
@@ -31,8 +36,8 @@ const Nav = () => {
               <Link href="/soon" className={`mx-8 relative transition-all duration-1000 ease-in-out ${isOpen ? 'left-0' : '-left-48'}`}>
                 <p className=' font-sans text-5xl text-[#40E0D0] p-0'>Register</p>
               </Link>
-              <Link href="/contact" className={`mx-8 relative transition-all duration-1000 ease-in-out ${isOpen ? 'left-0' : '-left-52'}`} >
-                <p className=' font-sans text-5xl text-[#40E0D0] p-0'>Contact</p>
+              <Link href="/about" className={`mx-8 relative transition-all duration-1000 ease-in-out ${isOpen ? 'left-0' : '-left-52'}`} >
+                <p className=' font-sans text-5xl text-[#40E0D0] p-0'>About Us</p>
               </Link>
             </div>
 
